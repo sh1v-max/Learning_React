0af1084faea403ae2cb4f0e7738d4c49cd0f4c1a
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-02-10 18:51:56 +0530

    react create element

--- Namaste_React/01_Inception/App.js
+++ Namaste_React/01_Inception/App.js
@@ -1,6 +1,10 @@
 //* how to create element using react
 // use React.createElement('tag name', { id: "heading"  here we give attributes}, 'content inside tag')
-const heading = React.createElement('h1', { id: "heading" /* here we give attributes*/}, 'Hello world from React')
+const heading = React.createElement(
+  'h1',
+  { id: 'heading' /* here we give attributes*/ },
+  'Hello world from React'
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
@@ -15,15 +19,15 @@ root.render(heading)
  *    <h1>i'm h1 tag</h1>
  *  </div>
  *</div>
-*/
+ */
 
 const parent = React.createElement(
-  "div",
-  {id: "parent"},
+  'div',
+  { id: 'parent' },
   React.createElement(
-    "div",
-    {id: "child"},
-    React.createElement("h1", {}, "i'm an h1 tag")
+    'div',
+    { id: 'child' },
+    React.createElement('h1', {}, "i'm an h1 tag")
   )
 )
 
@@ -40,3 +44,38 @@ root.render(parent)
 
 //* how to create siblings
 
+const parent2 = React.createElement(
+  'div',
+  { id: 'parent' },
+  React.createElement('div', { id: 'child' }, [
+    React.createElement('h1', {}, "i'm an h1 tag"),
+    React.createElement('h2', {}, "i'm an h2 tag"),
+  ])
+  // you wrap them inside an array
+)
+
+root.render(parent2)
+
+// other example
+// using array
+const parent3 = React.createElement(
+  'div',
+  { id: 'parent' },
+  [
+    React.createElement('div', { id: 'child' }, [
+      React.createElement('h1', {}, "i'm an h1 tag"),
+      React.createElement('h2', {}, "i'm an h2 tag"),
+    ]),
+    React.createElement('div', { id: 'child2' }, [
+      React.createElement('h1', {}, "i'm an h1 tag"),
+      React.createElement('h2', {}, "i'm an h2 tag"),
+    ]),
+  ]
+  // you wrap them inside an array
+)
+
+root.render(parent3)
+
+// it does make our code look hard to understand
+// that's why we use jsx, but this is the core of react
+// we will no longer be using React.createElement
