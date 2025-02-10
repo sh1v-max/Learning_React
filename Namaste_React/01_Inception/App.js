87e44d0c4cb04295aa7aae9d17d764ba13867318
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-02-10 16:31:04 +0530

    basics of react

--- Namaste_React/01_Inception/App.js
+++ Namaste_React/01_Inception/App.js
@@ -3,3 +3,24 @@ const heading = React.createElement('h1', { id: "heading" /* here we give attrib
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(heading)
+console.log(heading) // it will return an object
+// this was the simple syntax to create only 1 element
+
+//? how to create nested element
+/**
+ *<div class="parent">
+ *  <div class="child">
+ *    <h1>i'm h1 tag</h1>
+ *  </div>
+ *</div>
+*/
+
+const parent = React.createElement(
+  "div",
+  {id: "parent"},
+  React.createElement(
+    "div",
+    {id: "child"},
+    React.createElement("h1", {}, "i'm an h1 tag")
+  )
+)
