d969468e722c259ad76cdc4ef74816cc5463963c
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-02-15 18:28:07 +0530

    react elements

--- App.js
+++ App.js
@@ -2,3 +2,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React.createElement => creates a new element
+//* this creates a new react element and not a html element
+// which when renders on dom, gets converted to html element
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello, React!🚀"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
