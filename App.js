9050a5d0b03116c8fb5239e5aff25a08cf09bda2
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-02-15 22:53:16 +0530

    notes and pdfs

--- App.js
+++ App.js
@@ -12,6 +12,21 @@ const heading = React.createElement(
   "Hello, React!🚀"
 );
 
+//* same using 
+//? JSX is not HTML
+// JSX is HTML/XML like syntax extension for JavaScript
+// JSX is not a part of react
+// JSX is not a valid JavaScript
+// js engine can't understand JSX
+// but parcel < babel run in properly
+
+const jsxHeading = <h1 id="heading">Hello, React! using JSX🚀</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+// root.render(heading);
+root.render(jsxHeading);
+
+console.log(heading);
+console.log(jsxHeading);
+// both are objects and same
