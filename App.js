15a0a638c0625cdbd0a852a37b706c8e9d6c21a8
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-02-16 15:33:59 +0530

    jsx

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
 //* importing react
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 // React.createElement => creates a new element
 //* this creates a new react element and not a html element
 // which when renders on dom, gets converted to html element
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello, React!🚀"
-);
+const heading = React.createElement('h1', { id: 'heading' }, 'Hello, React!🚀')
 
-//* same using 
+//* same using
 //? JSX is not HTML
+//& babel transpile it to react
 // JSX is HTML/XML like syntax extension for JavaScript
 // JSX is not a part of react
 // JSX is not a valid JavaScript
@@ -21,7 +18,7 @@ const heading = React.createElement(
 // but parcel < babel run in properly
 
 // const jsxHeading = <h1 id="heading">Hello, React! using JSX🚀</h1>;
-const jsxHeading = <h1 className="head">Hello, React! using JSX🚀</h1>;
+const jsxHeading = <h1 className="head">Hello, React! using JSX🚀</h1>
 // giving class using jsx as className, but not class...... it's not html
 //? to add attribute in jsx, use camelCase
 
@@ -30,18 +27,13 @@ const heading0 = (
   <h1 id="h1" key="h1">
     This is JSX
   </h1>
-);
+)
 // for multiple line, we need to wrap in ()
 
-
-
-
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 // root.render(heading);
-root.render(jsxHeading);
+root.render(jsxHeading)
 
-console.log(heading);
-console.log(jsxHeading);
+console.log(heading)
+console.log(jsxHeading)
 // both are objects and same
