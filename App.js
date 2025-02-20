9f677a7bd1b13e95350a049a72697ccca57699ef
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-02-20 20:52:33 +0530

    config driven ut

--- App.js
+++ App.js
@@ -1877,131 +1877,131 @@ const restaurantList = [
   },
 ]
 
-const resObj = {
-  type: 'restaurant',
-  data: {
-    type: 'F',
-    id: '73011',
-    name: 'KFC',
-    uuid: '27ff4155-fe46-418e-9862-ab98953bf953',
-    city: '22',
-    area: 'Anand Vihar Colony',
-    totalRatingsString: '5000+ ratings',
-    cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
-    cuisines: ['American', 'Snacks', 'Biryani'],
-    tags: [],
-    costForTwo: 30000,
-    costForTwoString: '₹300 FOR TWO',
-    deliveryTime: 31,
-    minDeliveryTime: 31,
-    maxDeliveryTime: 31,
-    slaString: '31 MINS',
-    lastMileTravel: 6.199999809265137,
-    slugs: {
-      restaurant: 'kfc-chukkuwala-chukkuwala',
-      city: 'dehradun',
-    },
-    cityState: '22',
-    address:
-      'KFC Restaurant, Khasra No 1281/1291, Opp Dr Manish Jain hospital, Chakrata road, Dehradun-248001',
-    locality: 'Clock Tower',
-    parentId: 547,
-    unserviceable: false,
-    veg: false,
-    select: false,
-    favorite: false,
-    tradeCampaignHeaders: [],
-    aggregatedDiscountInfo: {
-      header: '50% off',
-      shortDescriptionList: [
-        {
-          meta: '50% off | Use WELCOME50',
-          discountType: 'Percentage',
-          operationType: 'RESTAURANT',
-        },
-      ],
-      descriptionList: [
-        {
-          meta: '50% off up to ₹100 | Use code WELCOME50',
-          discountType: 'Percentage',
-          operationType: 'RESTAURANT',
-        },
-      ],
-      subHeader: '',
-      headerType: 0,
-      superFreedel: '',
-    },
-    aggregatedDiscountInfoV2: {
-      header: '50% OFF',
-      shortDescriptionList: [
-        {
-          meta: 'Use WELCOME50',
-          discountType: 'Percentage',
-          operationType: 'RESTAURANT',
-        },
-      ],
-      descriptionList: [
-        {
-          meta: '50% off up to ₹100 | Use code WELCOME50',
-          discountType: 'Percentage',
-          operationType: 'RESTAURANT',
-        },
-      ],
-      subHeader: '',
-      headerType: 0,
-      superFreedel: '',
-    },
-    ribbon: [
-      {
-        type: 'PROMOTED',
-      },
-    ],
-    chain: [],
-    feeDetails: {
-      fees: [],
-      totalFees: 0,
-      message: '',
-      title: '',
-      amount: '',
-      icon: '',
-    },
-    availability: {
-      opened: true,
-      nextOpenMessage: '',
-      nextCloseMessage: '',
-    },
-    longDistanceEnabled: 0,
-    rainMode: 'NONE',
-    thirdPartyAddress: false,
-    thirdPartyVendor: '',
-    adTrackingID: 'cid=5698075~p=1~eid=00000185-8b09-7c92-0e51-a04b00dc0124',
-    badges: {
-      imageBased: [],
-      textBased: [],
-      textExtendedBadges: [],
-    },
-    lastMileTravelString: '6.1 kms',
-    hasSurge: false,
-    sla: {
-      restaurantId: '73011',
-      deliveryTime: 31,
-      minDeliveryTime: 31,
-      maxDeliveryTime: 31,
-      lastMileTravel: 6.199999809265137,
-      lastMileDistance: 0,
-      serviceability: 'SERVICEABLE',
-      rainMode: 'NONE',
-      longDistance: 'NOT_LONG_DISTANCE',
-      preferentialService: false,
-      iconType: 'EMPTY',
-    },
-    promoted: true,
-    avgRating: '4.0',
-    totalRatings: 5000,
-    new: false,
-  },
-  subtype: 'basic',
-}
+// const resObj = {
+//   type: 'restaurant',
+//   data: {
+//     type: 'F',
+//     id: '73011',
+//     name: 'KFC',
+//     uuid: '27ff4155-fe46-418e-9862-ab98953bf953',
+//     city: '22',
+//     area: 'Anand Vihar Colony',
+//     totalRatingsString: '5000+ ratings',
+//     cloudinaryImageId: 'bdcd233971b7c81bf77e1fa4471280eb',
+//     cuisines: ['American', 'Snacks', 'Biryani'],
+//     tags: [],
+//     costForTwo: 30000,
+//     costForTwoString: '₹300 FOR TWO',
+//     deliveryTime: 31,
+//     minDeliveryTime: 31,
+//     maxDeliveryTime: 31,
+//     slaString: '31 MINS',
+//     lastMileTravel: 6.199999809265137,
+//     slugs: {
+//       restaurant: 'kfc-chukkuwala-chukkuwala',
+//       city: 'dehradun',
+//     },
+//     cityState: '22',
+//     address:
+//       'KFC Restaurant, Khasra No 1281/1291, Opp Dr Manish Jain hospital, Chakrata road, Dehradun-248001',
+//     locality: 'Clock Tower',
+//     parentId: 547,
+//     unserviceable: false,
+//     veg: false,
+//     select: false,
+//     favorite: false,
+//     tradeCampaignHeaders: [],
+//     aggregatedDiscountInfo: {
+//       header: '50% off',
+//       shortDescriptionList: [
+//         {
+//           meta: '50% off | Use WELCOME50',
+//           discountType: 'Percentage',
+//           operationType: 'RESTAURANT',
+//         },
+//       ],
+//       descriptionList: [
+//         {
+//           meta: '50% off up to ₹100 | Use code WELCOME50',
+//           discountType: 'Percentage',
+//           operationType: 'RESTAURANT',
+//         },
+//       ],
+//       subHeader: '',
+//       headerType: 0,
+//       superFreedel: '',
+//     },
+//     aggregatedDiscountInfoV2: {
+//       header: '50% OFF',
+//       shortDescriptionList: [
+//         {
+//           meta: 'Use WELCOME50',
+//           discountType: 'Percentage',
+//           operationType: 'RESTAURANT',
+//         },
+//       ],
+//       descriptionList: [
+//         {
+//           meta: '50% off up to ₹100 | Use code WELCOME50',
+//           discountType: 'Percentage',
+//           operationType: 'RESTAURANT',
+//         },
+//       ],
+//       subHeader: '',
+//       headerType: 0,
+//       superFreedel: '',
+//     },
+//     ribbon: [
+//       {
+//         type: 'PROMOTED',
+//       },
+//     ],
+//     chain: [],
+//     feeDetails: {
+//       fees: [],
+//       totalFees: 0,
+//       message: '',
+//       title: '',
+//       amount: '',
+//       icon: '',
+//     },
+//     availability: {
+//       opened: true,
+//       nextOpenMessage: '',
+//       nextCloseMessage: '',
+//     },
+//     longDistanceEnabled: 0,
+//     rainMode: 'NONE',
+//     thirdPartyAddress: false,
+//     thirdPartyVendor: '',
+//     adTrackingID: 'cid=5698075~p=1~eid=00000185-8b09-7c92-0e51-a04b00dc0124',
+//     badges: {
+//       imageBased: [],
+//       textBased: [],
+//       textExtendedBadges: [],
+//     },
+//     lastMileTravelString: '6.1 kms',
+//     hasSurge: false,
+//     sla: {
+//       restaurantId: '73011',
+//       deliveryTime: 31,
+//       minDeliveryTime: 31,
+//       maxDeliveryTime: 31,
+//       lastMileTravel: 6.199999809265137,
+//       lastMileDistance: 0,
+//       serviceability: 'SERVICEABLE',
+//       rainMode: 'NONE',
+//       longDistance: 'NOT_LONG_DISTANCE',
+//       preferentialService: false,
+//       iconType: 'EMPTY',
+//     },
+//     promoted: true,
+//     avgRating: '4.0',
+//     totalRatings: 5000,
+//     new: false,
+//   },
+//   subtype: 'basic',
+// }
 
 // const RestaurantCard = (props) => {
 //   console.log(props)
@@ -2037,34 +2037,56 @@ const resObj = {
 //   )
 // }
 
-const RestaurantCard = (props) => {
-  const { resData } = props
+const RestaurantCard = ({
+  // cloudinaryImageId,
+  name,
+  cuisines,
+  area,
+  lastMileTravelString,
+  costForTwoString,
+  avgRating,
+  deliveryTime,
+}) => {
   return (
-    <div className="res-card" style={{ backgroundColor: '#f0f0f0' }}>
+    <div className="res-card">
       <img
         className="res-logo"
         alt="res-logo"
         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rvxp5xbniat84r6efku2"
       />
-      <h3>{resData.data.name} </h3>
-      <h4>{resData.data.cuisines.join(", ")} </h4>
-      <h4>{resData.data.avgRating} Star</h4>
-      <h4>{resData.data.costForTwo / 100} for TWO</h4>
-      <h4>{resData.data.deliveryTime} minutes</h4>
+      <h2>{name}</h2>
+      <h4>{cuisines.join(', ')}</h4>
+      <h4>{area}</h4>
+      <span>
+        <h4> Rating : {avgRating} Stars</h4>
+        <h4>{lastMileTravelString}</h4>
+        <h5>{deliveryTime} Min</h5>
+        <h5>{costForTwoString}</h5>
+      </span>
     </div>
   )
 }
 
+// const Body = () => {
+//   return (
+//     <div className="body">
+//       <div className="search">Search</div>
+//       <div className="res-container">
+//         {/* <RestaurantCard resData = {resObj}/>
+//         <RestaurantCard /> */}
+
+//       </div>
+//     </div>
+//   )
+// }
+
+//? using map function to render multiple components
 const Body = () => {
   return (
-    <div className="body">
-      <div className="search">Search</div>
-      <div className="res-container">
-        {/* <RestaurantCard resData = {resObj}/>
-        <RestaurantCard /> */}
-        {/* Using map */}
-        
-      </div>
+    <div className="res-container">
+      {restaurantList.map((restaurant) => {
+        return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+      })}
     </div>
   )
 }
@@ -2086,11 +2108,16 @@ const Body = () => {
 //   )
 // }
 
+const Footer = () => {
+  return <h4>Footer</h4>
+}
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
       <Body />
+      <Footer />
     </div>
   )
 }
