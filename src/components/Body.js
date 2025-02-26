a7ffcce9885f2a768071c22a5a77e5e5250b3248
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-02-26 18:52:42 +0530

    events

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,8 +4,15 @@ import resList from '../utils/mockData'
 const Body = () => {
   return (
     <div className="body">
-      <div className='filter'>
-        <button className='filter-btn'>Top Rated Restaurant</button>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log('Button clicked')
+          }}
+        >
+          Top Rated Restaurant
+        </button>
       </div>
       <div className="res-container">
         {resList.map((restaurant) => {
