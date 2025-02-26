5bae015c3f2fbbe7f8b795c000156cc059354ddc
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-02-26 22:23:43 +0530

    react hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,88 +4,172 @@ import { useState } from 'react'
 
 const Body = () => {
 
-  let listOfRestaurants = [
-    {
-      type: 'restaurant',
-      data: {
-        type: 'F',
-        id: '81093',
-        name: 'Dominos',
-        uuid: '40b0b55b-e9af-43e7-aeae-be1b012d0b1d',
-        city: '21',
-        area: 'Athwa',
-        totalRatingsString: '500+ ratings',
-        cloudinaryImageId: 'nyp7yrzwc1dc2xqfkydk',
-        cuisines: ['Ice Cream'],
-        tags: [],
-        costForTwo: 25000,
-        costForTwoString: '₹250 FOR TWO',
-        deliveryTime: 30,
-        minDeliveryTime: 30,
-        maxDeliveryTime: 40,
-        slaString: '30-40 MINS',
-        lastMileTravel: 5.599999904632568,
-        avgRating: 4.5,
-      },
-    },
-    {
-      type: 'restaurant',
-      data: {
-        type: 'F',
-        id: '81094',
-        name: 'MCD',
-        uuid: '40b0b55b-e9af-43e7-aeae-be1b012d0b1d',
-        city: '21',
-        area: 'Athwa',
-        totalRatingsString: '500+ ratings',
-        cloudinaryImageId: 'nyp7yrzwc1dc2xqfkydk',
-        cuisines: ['Ice Cream'],
-        tags: [],
-        costForTwo: 25000,
-        costForTwoString: '₹250 FOR TWO',
-        deliveryTime: 30,
-        minDeliveryTime: 30,
-        maxDeliveryTime: 40,
-        slaString: '30-40 MINS',
-        lastMileTravel: 5.599999904632568,
-        avgRating: 3.5,
-      },
-    },
-    {
-      type: 'restaurant',
-      data: {
-        type: 'F',
-        id: '81092',
-        name: 'KFC',
-        uuid: '40b0b55b-e9af-43e7-aeae-be1b012d0b1d',
-        city: '21',
-        area: 'Athwa',
-        totalRatingsString: '500+ ratings',
-        cloudinaryImageId: 'nyp7yrzwc1dc2xqfkydk',
-        cuisines: ['Ice Cream'],
-        tags: [],
-        costForTwo: 25000,
-        costForTwoString: '₹250 FOR TWO',
-        deliveryTime: 30,
-        minDeliveryTime: 30,
-        maxDeliveryTime: 40,
-        slaString: '30-40 MINS',
-        lastMileTravel: 5.599999904632568,
-        avgRating: 4.1,
-      },
-    },
-  ]
+  // let listOfRestaurants = [
+  //   {
+  //     type: 'restaurant',
+  //     data: {
+  //       type: 'F',
+  //       id: '81093',
+  //       name: 'Dominos',
+  //       uuid: '40b0b55b-e9af-43e7-aeae-be1b012d0b1d',
+  //       city: '21',
+  //       area: 'Athwa',
+  //       totalRatingsString: '500+ ratings',
+  //       cloudinaryImageId: 'nyp7yrzwc1dc2xqfkydk',
+  //       cuisines: ['Ice Cream'],
+  //       tags: [],
+  //       costForTwo: 25000,
+  //       costForTwoString: '₹250 FOR TWO',
+  //       deliveryTime: 30,
+  //       minDeliveryTime: 30,
+  //       maxDeliveryTime: 40,
+  //       slaString: '30-40 MINS',
+  //       lastMileTravel: 5.599999904632568,
+  //       avgRating: 4.5,
+  //     },
+  //   },
+  //   {
+  //     type: 'restaurant',
+  //     data: {
+  //       type: 'F',
+  //       id: '81094',
+  //       name: 'MCD',
+  //       uuid: '40b0b55b-e9af-43e7-aeae-be1b012d0b1d',
+  //       city: '21',
+  //       area: 'Athwa',
+  //       totalRatingsString: '500+ ratings',
+  //       cloudinaryImageId: 'nyp7yrzwc1dc2xqfkydk',
+  //       cuisines: ['Ice Cream'],
+  //       tags: [],
+  //       costForTwo: 25000,
+  //       costForTwoString: '₹250 FOR TWO',
+  //       deliveryTime: 30,
+  //       minDeliveryTime: 30,
+  //       maxDeliveryTime: 40,
+  //       slaString: '30-40 MINS',
+  //       lastMileTravel: 5.599999904632568,
+  //       avgRating: 3.5,
+  //     },
+  //   },
+  //   {
+  //     type: 'restaurant',
+  //     data: {
+  //       type: 'F',
+  //       id: '81092',
+  //       name: 'KFC',
+  //       uuid: '40b0b55b-e9af-43e7-aeae-be1b012d0b1d',
+  //       city: '21',
+  //       area: 'Athwa',
+  //       totalRatingsString: '500+ ratings',
+  //       cloudinaryImageId: 'nyp7yrzwc1dc2xqfkydk',
+  //       cuisines: ['Ice Cream'],
+  //       tags: [],
+  //       costForTwo: 25000,
+  //       costForTwoString: '₹250 FOR TWO',
+  //       deliveryTime: 30,
+  //       minDeliveryTime: 30,
+  //       maxDeliveryTime: 40,
+  //       slaString: '30-40 MINS',
+  //       lastMileTravel: 5.599999904632568,
+  //       avgRating: 4.1,
+  //     },
+  //   },
+  // ]
+
+  //? using useState to store the list of restaurants
+  // const [listOfRestaurants, setListOfRestaurants] = useState([
+  //   {
+  //     type: 'restaurant',
+  //     data: {
+  //       type: 'F',
+  //       id: '81093',
+  //       name: 'Dominos',
+  //       uuid: '40b0b55b-e9af-43e7-aeae-be1b012d0b1d',
+  //       city: '21',
+  //       area: 'Athwa',
+  //       totalRatingsString: '500+ ratings',
+  //       cloudinaryImageId: 'nyp7yrzwc1dc2xqfkydk',
+  //       cuisines: ['Ice Cream'],
+  //       tags: [],
+  //       costForTwo: 25000,
+  //       costForTwoString: '₹250 FOR TWO',
+  //       deliveryTime: 30,
+  //       minDeliveryTime: 30,
+  //       maxDeliveryTime: 40,
+  //       slaString: '30-40 MINS',
+  //       lastMileTravel: 5.599999904632568,
+  //       avgRating: 4.5,
+  //     },
+  //   },
+  //   {
+  //     type: 'restaurant',
+  //     data: {
+  //       type: 'F',
+  //       id: '81094',
+  //       name: 'MCD',
+  //       uuid: '40b0b55b-e9af-43e7-aeae-be1b012d0b1d',
+  //       city: '21',
+  //       area: 'Athwa',
+  //       totalRatingsString: '500+ ratings',
+  //       cloudinaryImageId: 'nyp7yrzwc1dc2xqfkydk',
+  //       cuisines: ['Ice Cream'],
+  //       tags: [],
+  //       costForTwo: 25000,
+  //       costForTwoString: '₹250 FOR TWO',
+  //       deliveryTime: 30,
+  //       minDeliveryTime: 30,
+  //       maxDeliveryTime: 40,
+  //       slaString: '30-40 MINS',
+  //       lastMileTravel: 5.599999904632568,
+  //       avgRating: 3.5,
+  //     },
+  //   },
+  //   {
+  //     type: 'restaurant',
+  //     data: {
+  //       type: 'F',
+  //       id: '81092',
+  //       name: 'KFC',
+  //       uuid: '40b0b55b-e9af-43e7-aeae-be1b012d0b1d',
+  //       city: '21',
+  //       area: 'Athwa',
+  //       totalRatingsString: '500+ ratings',
+  //       cloudinaryImageId: 'nyp7yrzwc1dc2xqfkydk',
+  //       cuisines: ['Ice Cream'],
+  //       tags: [],
+  //       costForTwo: 25000,
+  //       costForTwoString: '₹250 FOR TWO',
+  //       deliveryTime: 30,
+  //       minDeliveryTime: 30,
+  //       maxDeliveryTime: 40,
+  //       slaString: '30-40 MINS',
+  //       lastMileTravel: 5.599999904632568,
+  //       avgRating: 4.1,
+  //     },
+  //   },
+  // ])
+  
+  //? now using our mock data
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList)
   
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
+          // onClick={() => {
+          //   listOfRestaurants = listOfRestaurants.filter(
+          //     (res) => res.data.avgRating > 4
+          //   )
+          //   console.log(listOfRestaurants)
+          // }}
+          //? using useState to update the list of restaurants
           onClick={() => {
-            listOfRestaurants = listOfRestaurants.filter(
+            const filteredList = listOfRestaurants.filter(
               (res) => res.data.avgRating > 4
             )
-            console.log(listOfRestaurants)
+            setListOfRestaurants(filteredList)
+            console.log(filteredList)
           }}
         >
           Top Rated Restaurant
