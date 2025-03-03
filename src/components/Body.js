3675a966efdafc06d69ab902f8a91f300bbcf0e1
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-03-03 21:44:21 +0530

    fetching data from swiggy

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,7 +5,17 @@ import { useState, useEffect } from 'react'
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState(resList)
 
-  
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    )
+    const json = await data.json()
+    console.log(json)
+  }
   
   return (
     <div className="body">
