8b11aacfd72e1fdb1dfa8271da40746c81f5518b
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-03-04 19:19:10 +0530

    solving fetch issue

--- src/components/Body.js
+++ src/components/Body.js
@@ -15,6 +15,7 @@ const Body = () => {
     )
     const json = await data.json()
     console.log(json)
+    console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants[3])
   }
   
   return (
