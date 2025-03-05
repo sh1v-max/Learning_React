adafaf533ed25e2a5dbe554d1546b9d1f103458f
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-03-05 18:25:45 +0530

    styling

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://static.vecteezy.com/system/resources/previews/050/585/647/non_2x/a-hamburger-and-a-drink-are-shown-in-a-line-free-vector.jpg"
+          alt="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+
+const Footer = () => {
+  return <h4 className='footer'>Footer</h4>
+}
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      {/* <Body /> */}
+      <Footer />
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<AppLayout />)
