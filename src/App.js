9aa8c4fbd01bedb628d9b517ffffb22e78dc06e0
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-03-05 17:48:50 +0530

    restart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './components/Header' // will work fine without .js extension
-import Body from './components/Body'
 
 const Footer = () => {
   return <h4>Footer</h4>
--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const CDN_URL =
-  'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rvxp5xbniat84r6efku2'
-
-export const LOGO_URL =
-  'https://img.freepik.com/premium-vector/food-logo-design-with-leaf-creative-concept-premium-vector_526458-2675.jpg?semt=ais_hybrid'
-
-// we won't be using default export
-//! named export
