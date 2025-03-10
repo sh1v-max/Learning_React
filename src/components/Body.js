c11950e7b845c9fc9929a2e48c920326de0dbfa1
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-03-10 19:29:02 +0530

    swiggy api

--- src/components/Body.js
+++ src/components/Body.js
@@ -82,7 +82,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      'https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING'
+      'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
     )
 
     const json = await data.json()
