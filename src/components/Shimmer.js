b00d7e6c09f6ba194f85f8d57eec79558763e320
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-03-11 13:39:28 +0530

    shimmer

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,16 +1,24 @@
 const Shimmer = () => {
+  // return (
+  //   <div className="shimmer-wrapper">
+  //     {Array(10)
+  //       .fill("")
+  //       .map((_, index) => (
+  //         <div key={index} className="shimmer-card">
+  //           <div className="shimmer-img"></div>
+  //           <div className="shimmer-text long"></div>
+  //           <div className="shimmer-text medium"></div>
+  //           <div className="shimmer-text short"></div>
+  //         </div>
+  //       ))}
+  //   </div>
+  // );
+
   return (
-    <div className="shimmer-wrapper">
-      {Array(10)
-        .fill("")
-        .map((_, index) => (
-          <div key={index} className="shimmer-card">
-            <div className="shimmer-img"></div>
-            <div className="shimmer-text long"></div>
-            <div className="shimmer-text medium"></div>
-            <div className="shimmer-text short"></div>
-          </div>
-        ))}
+    <div className="shimmer-container">
+      <div className="shimmer-cards">Cards</div>
+      <div className="shimmer-cards">Cards</div>
+      <div className="shimmer-cards">Cards</div>
     </div>
   );
 };
