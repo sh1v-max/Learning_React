6943e05c6912e1fd55edeaeb1645e588d472f34b
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-03-15 19:26:38 +0530

    error

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,21 +1,23 @@
-import Header from "./Header";
-import Footer from "./Footer";
-import { useRouteError } from "react-router";
+import Header from './Header'
+import Footer from './Footer'
+import { useRouteError } from 'react-router'
 
 const Error = () => {
-  const err = useRouteError();
-  console.log(err);
+  const err = useRouteError()
+  console.log(err)
   return (
     <div className="error-container">
       <Header />
       <main className="error-content">
-        <h1>Oops! Requested URL not found</h1>
+        <h1>Oops!</h1>
         <h2>Something went wrong</h2>
-        <p>404 Error.</p>
+        <p>
+          {err.status}: {err.statusText}
+        </p>
       </main>
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default Error;
+export default Error
