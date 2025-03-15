79dabc48bbe03ec5a1ccc2be7eb26f715fcc7231
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-03-15 22:07:07 +0530

    linking instead of anchor tagh

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import Contact from './components/Contact'
 import Error from './components/Error'
 // to create a route, we need to import createBrowserRouter from react-router-dom
 // version matters, make sure you are using the correct version
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      {/* Outlet is a placeholder for the children of the route */}
+      <Outlet />
       <Footer />
     </div>
   )
@@ -23,17 +24,25 @@ const AppLayout = () => {
 // utilize createBrowserRouter to create a router
 const appRouter = createBrowserRouter([
   {
+    // creating children in the root route
     path: '/',
     element: <AppLayout />,
+    children :[
+      {
+        path: '/',
+        element: <Body />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+      // about and contact are the children of the root route, ie appLayout
+    ],
     errorElement: <Error/>,
-  },
-  {
-    path: '/about',
-    element: <About />,
-  },
-  {
-    path: '/contact',
-    element: <Contact />,
   },
 ])
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,41 @@
-import { useEffect, useState } from 'react'
 import { LOGO_URL } from '../utils/constants'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState('Login')
 
   // console.log("rendered")
-  
+
   //? useEffect
   // if there's no dependency array, useEffect will run on every render
   // if dependency array is empty ("[]"), useEffect will run only once
-  // if there is dependency array with some values ("[btnNameReact]"), 
+  // if there is dependency array with some values ("[btnNameReact]"),
   // useEffect will run when those values change, in this case, every time btnNameReact changes
   useEffect(() => {
     // console.log("useEffect called")
   }, [btnNameReact]) // empty dependency array
-  
+
   return (
     <div className="header">
       <div className="logo-container">
-        <img className="logo" src={LOGO_URL} alt="logo" />
+        <Link to="/"><img className="logo" src={LOGO_URL} alt="logo" /></Link>
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
+          <li>
+            <Link to = "/">Home</Link>
+          </li>
+          <li>
+            <Link to = "/about">About</Link>
+          </li>
+          <li>
+            <Link to = "/contact">Contact Us</Link></li>
+          <li>
+            <Link to = "">Cart</Link>
+          </li>
+          {/* we wont be using anchor tag to link to different pages */}
+          {/* as it will refresh the page, slow down the performance and we will lose the state */}
           <button
             className="login-btn"
             onClick={() => {
