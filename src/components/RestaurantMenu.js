28495cdd444335af6543309db0035c9127dd0a90
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-03-16 17:45:08 +0530

    creating restaurant menu card

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,31 +1,42 @@
-import Shimmer from "./Shimmer"
-import { useEffect, useState } from "react"
+import Shimmer from './Shimmer'
+import { useEffect, useState } from 'react'
 
 const RestaurantMenu = () => {
-
   const [resInfo, setResInfo] = useState(null)
-  
+
   useEffect(() => {
     fetchData()
   }, [])
-  
+
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=43836&catalog_qa=undefined&submitAction=ENTER"
+      'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=43836&catalog_qa=undefined&submitAction=ENTER'
     )
     const json = await data.json()
     console.log(json.data?.cards[2]?.card?.card?.info)
-    setResInfo(json.data)
+    setResInfo(json?.data)
   }
 
-  // const {name} = resInfo
-  // if (resInfo === null) {
-  //   return <Shimmer/>
-  // }
-  
-  return resInfo === null? <Shimmer/> : (
+  const { 
+    name, 
+    avgRating, 
+    areaName, 
+    costForTwoMessage, 
+    cuisines, 
+    sla, 
+    totalRatingsString
+  } =  resInfo?.cards[2]?.card?.card?.info || 'Loading...'
+
+  return resInfo === null ? (
+    <Shimmer />
+  ) : (
     <div className="menu">
-      <h1>{resInfo?.cards[2]?.card?.card?.info.name}</h1>
+      <h1>{name}</h1>
+      <h4>{avgRating}⭐ ({totalRatingsString})</h4>
+      <h4>{costForTwoMessage}</h4>
+      <h4>Cuisines:- {cuisines.join(", ")}</h4>
+      <h4>Delivery time:- {sla.slaString}</h4>
+      <h4>{}</h4>
       <h2>Menu</h2>
       <ul>
         <li>Biryani</li>
