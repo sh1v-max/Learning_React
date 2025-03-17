9fe7c23cbdde88f9a735e5c2404a444cdfc0078c
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-03-17 18:24:58 +0530

    menu api

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -15,7 +15,7 @@ const RestaurantMenu = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=694838&catalog_qa=undefined&submitAction=ENTER'
+      694838&catalog_qa=undefined&submitAction=ENTER'
     )
     const json = await data.json()
     // console.log(json.data?.cards[2]?.card?.card?.info)
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,7 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 export const LOGO_URL = "https://static.vecteezy.com/system/resources/previews/050/585/647/non_2x/a-hamburger-and-a-drink-are-shown-in-a-line-free-vector.jpg"
 
+export MENU_API = "'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId="
+
 // we won't be using default export
 //! named export
