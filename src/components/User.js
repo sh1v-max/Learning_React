7efa130c151107074d1e256a2c17d6853212f645
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-03-22 13:29:22 +0530

    user created

---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = () => {
+  return (
+    <div className="user-card">
+      <h2>Name: Shiv</h2>
+      <h3>Location: varanasi</h3>
+      <h4>Contact: wazir@gmail.com</h4>
+    </div>
+  )
+}
+
+export default User;
