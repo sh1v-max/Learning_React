60a960db77ce9d51adc25a3195f0e5654121a833
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-03-22 16:12:50 +0530

    notes added

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,10 +5,16 @@ import '../css/User.css'
 //? that returns a piece of JSX
 class UserClass extends React.Component {
   // we need to import React from 'react' to use React.Component
+
+  //& to receive the prop, we need constructor
+  constructor(props) {
+    super(props)
+  }
+  
   render() {
     return (
       <div className="user-card">
-        <h2>Name: Shiv</h2>
+        <h2>Name: {this.props.name}</h2>
         <h3>Location: varanasi</h3>
         <h4>Contact: wazir@gmail.com</h4>
       </div>
