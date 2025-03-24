d0732af2904617f45f6496e33be92fc19a4b8fcf
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-03-24 17:36:59 +0530

    utility functions

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -15,7 +15,6 @@ const RestaurantMenu = () => {
   const fetchData = async () => {
     const data = await fetch(MENU_API + resId)
     const json = await data.json()
-
     setResInfo(json?.data)
   }
 
