f62a74eda8124f056eee34deb486c6bc2c6c20aa
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-03-26 17:37:18 +0530

    tailwind css

--- src/components/About.js
+++ src/components/About.js
@@ -3,22 +3,26 @@ import { ABOUT_IMG } from '../utils/constants'
 import User from './User'
 import UserClass from './UserClass'
 import AboutClass from './AboutClass'
-import "../css/About.css"
+import '../css/About.css'
 
 const About = () => {
   return (
     <div className="about-container">
+      <h1 className="text-3xl font-bold underline bg-red-500">Hello</h1>
       <div className="about-detail">
         <h1>
-          Welcome to Bite<span>Swift</span><br /> The world of <br />
-          <span className='tag'>Tasty & Fresh Food</span>
+          Welcome to Bite<span>Swift</span>
+          <br /> The world of <br />
+          <span className="tag">Tasty & Fresh Food</span>
         </h1>
         <h4>
-          "Better you will feel if you eat a Bite<span>Swift</span> healthy meal"
+          "Better you will feel if you eat a Bite<span>Swift</span> healthy
+          meal"
         </h4>
         <p>
-          At BiteSwift, we bring you the best food from top restaurants, ensuring quick delivery and fresh flavors.
-          Whether you're craving a spicy biryani, a cheesy pizza, or a healthy salad, we've got it all!
+          At BiteSwift, we bring you the best food from top restaurants,
+          ensuring quick delivery and fresh flavors. Whether you're craving a
+          spicy biryani, a cheesy pizza, or a healthy salad, we've got it all!
         </p>
         <h2>Why Choose Us?</h2>
         <ul>
@@ -29,18 +33,15 @@ const About = () => {
           <li>🌍 Serving Across Multiple Cities</li>
         </ul>
         <p>
-          Experience the best of dining at home with BiteSwift. Order now and enjoy a hassle-free food journey!
+          Experience the best of dining at home with BiteSwift. Order now and
+          enjoy a hassle-free food journey!
         </p>
         <h3>Contact Us</h3>
         <p>Email: support@biteswift.com</p>
         <p>Phone: +91 9369996021 / 7068900427</p>
       </div>
       <div className="about-img">
-        <img
-          className='image'
-          src={ ABOUT_IMG }
-          alt="Food Image"
-        />
+        <img className="image" src={ABOUT_IMG} alt="Food Image" />
       </div>
     </div>
     // <div>
