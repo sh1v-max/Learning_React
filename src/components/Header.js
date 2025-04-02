c898a6aed3ffa604dac425dcb94a5a48c79163e3
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-04-02 19:16:42 +0530

    index.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,52 +10,52 @@ const Header = () => {
 
   useEffect(() => {}, [btnNameReact])
 
-  // return (
-  //   <div className="header">
-  //     <div className="logo-container">
-  //       <Link to="/">
-  //         <img className="logo" src={LOGO_URL} alt="logo" />
-  //       </Link>
-  //     </div>
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <Link to="/">
+          <img className="logo" src={LOGO_URL} alt="logo" />
+        </Link>
+      </div>
 
-  //     {/* Hamburger Menu Icon */}
-  //     <button className="menu-icon" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-  //       ☰
-  //     </button>
+      {/* Hamburger Menu Icon */}
+      <button className="menu-icon" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        ☰
+      </button>
 
-  //     {/* Navigation Items */}
-  //     <div className={`nav-items ${isMenuOpen ? "open" : ""}`}>
-  //       <ul>
-  //         <li>
-  //           {onlineStatus ? "🟢" : "🔴"}
-  //         </li>
-  //         <li>
-  //           <Link to="/">Home</Link>
-  //         </li>
-  //         <li>
-  //           <Link to="/grocery">Grocery</Link>
-  //         </li>
-  //         <li>
-  //           <Link to="/about">About</Link>
-  //         </li>
-  //         <li>
-  //           <Link to="/contact">Contact Us</Link>
-  //         </li>
-  //         <li>
-  //           <Link to="/">Cart</Link>
-  //         </li>
-  //         <button
-  //           className="login-btn"
-  //           onClick={() =>
-  //             setBtnNameReact(btnNameReact === "Login" ? "Logout" : "Login")
-  //           }
-  //         >
-  //           {btnNameReact}
-  //         </button>
-  //       </ul>
-  //     </div>
-  //   </div>
-  // );
+      {/* Navigation Items */}
+      <div className={`nav-items ${isMenuOpen ? "open" : ""}`}>
+        <ul>
+          <li>
+            {onlineStatus ? "🟢" : "🔴"}
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/grocery">Grocery</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/">Cart</Link>
+          </li>
+          <button
+            className="login-btn"
+            onClick={() =>
+              setBtnNameReact(btnNameReact === "Login" ? "Logout" : "Login")
+            }
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
 
   // with tailwindcss
 
