f7e9c4142e348ed3525ab119a5c3d9666cb232c9
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-04-03 22:58:30 +0530

    created discount offer restaurant card

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -49,11 +49,15 @@ export const withDiscountOffer = (RestaurantCard) => {
     const { aggregatedDiscountInfoV3 } = props
 
     return (
-      <div className="w-[250px] h-[300px] bg-white rounded-[8px] shadow-md cursor-pointer overflow-hidden hover:scale-[0.98] relative">
+      // <div className="w-[250px] h-[300px] bg-white rounded-[8px] shadow-md cursor-pointer overflow-hidden hover:scale-[0.98] relative">
+      <div className="discount-container">
+        {/* Check if discount info is available and render it */}
         {aggregatedDiscountInfoV3 && (
-          <div className="text-gray-50 text-xl font-extrabold absolute top-[55%] left-[2px]">{`${aggregatedDiscountInfoV3.header} ${aggregatedDiscountInfoV3.subHeader}`}</div>
+          // <div className="text-gray-50 text-xl font-extrabold absolute top-[55%] left-[2px]">{`${aggregatedDiscountInfoV3.header} ${aggregatedDiscountInfoV3.subHeader}`}</div>
+          <div className="discount">{`${aggregatedDiscountInfoV3.header} ${aggregatedDiscountInfoV3.subHeader}`}</div>
         )}
         <RestaurantCard {...props} />
+        {/* Render the original RestaurantCard with all its props */}
       </div>
     )
   }
