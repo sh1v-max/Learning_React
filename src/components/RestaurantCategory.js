56d81482d48ac8e14162860df4d0a851efe48b1f
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-04-07 23:00:48 +0530

    ItemList done

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,13 +1,18 @@
+import ItemList from "./ItemList";
+
 const RestaurantCategory = ({data}) => {
 
   console.log(data)
   
   return (
     <div className="">
+      {/* Accordion Header */}
       <div className="flex border-b border-gray-200 justify-between items-center my-4 py-4 px-2 shadow-lg bg-gray-100 cursor-pointer">
         <span className="text-l font-extrabold">{data.title} ({data?.itemCards?.length})</span>
         <span>⬇️</span>
       </div>
+      {/* Accordion Body */}
+      <ItemList items = {data?.itemCards}/>
     </div>
   )
   
