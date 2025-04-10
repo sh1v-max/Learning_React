c87d7db25bd119f4728148a0cd21be736c7250f9
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-04-10 22:42:44 +0530

    adding items into the cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,30 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from 'react-redux'
 // import ItemList from "./ItemList"
-import ItemList from "./ItemList"
+import ItemList from './ItemList'
+// import { clearCart } from '../utils/cartSlice'
+import { clearCart } from '../utils/cartSlice'
 
-const Cart = ()=> {
-
-  const cartItems = useSelector((store)=>store.cart.items)
+const Cart = () => {
+  const cartItems = useSelector((store) => store.cart.items)
   console.log(cartItems)
+
+  const dispatch = useDispatch()
+  const handleClearCart = () => {
+    dispatch(clearCart())
+  }
   
   return (
     <div className="text-center m-4 p-4 ">
       <h1 className="text-4xl font-bold">Cart</h1>
       <div className="w-6/12 mx-auto">
-        <ItemList items={cartItems}/>
+        <button
+          className="bg-[orange] text-white font-bold py-2 px-4 rounded-lg cursor-pointer hover:bg-[green]"
+          onClick={handleClearCart}
+        >
+          Clear Cart
+        </button>
+        {cartItems.length === 0 && <h1 className="text-2xl font-bold">Cart is Empty please add some items</h1>}
+        <ItemList items={cartItems} />
       </div>
     </div>
   )
